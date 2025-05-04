4efd2eceb941e8ca743cf2464e5eb829da1099ef
laerciogil <laerciogilpb@gmail.com>
2025-05-04 16:05:10 -0300

    changed gradient descent and cost functions as proposed in the progamming assignment

--- w05-ex1-linear-regression/computeCost.m
+++ w05-ex1-linear-regression/computeCost.m
@@ -6,16 +6,16 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+h_theta = X * theta;          % hypotheses values for each example in X given theta
+sq_err = (h_theta - y) .^ 2;  % squares of element-wize diference of h and y
+J = sum(sq_err) / (2 * m);    % sum of sq. errors over 2 times m. (Cost)
 
 % =========================================================================
 
